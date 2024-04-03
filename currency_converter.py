a6b6af277d399c23a0e36d35156ae32049e05c7e
trice <160309591+trwit@users.noreply.github.com>
2024-04-03 12:33:46 +0900

    Add files via upload

---
+++ currency_converter.py
@@ -0,0 +1,6 @@
+# produced in 2024-04-03
+# Currency Converter
+
+won = float(input("Please enter the amount of Korean won you want to exchange.>"))
+dollar = won*1348.24
+print(dollar), print("dollar")
