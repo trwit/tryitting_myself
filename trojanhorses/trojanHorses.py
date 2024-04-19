2610653458cedd508444784b05865b55619d56f7
trice <160309591+trwit@users.noreply.github.com>
2024-04-19 09:10:35 +0900

    Add files via upload

---
+++ trojanhorses/trojanHorses.py
@@ -0,0 +1,8 @@
+print("Do you want to play the game 'Path to Millionaire'?")
+print('Pleas type "Y" or "N".')
+select = input()
+if 'Y' == select:
+    while(True):
+        print("?")
+if 'N' == select:
+    print("bye")
